5933089a6d67bd7e0431e099941fa9af08ddf1d2
前田 <sloth19920731@gmail.com>
2021-05-15 19:22:11 +0900

    main関数で試した時の余分なソース削除

--- src/main/java/com/suita/tarumi/SpringNihaoMemo/SpringNihaoMemoApplication.java
+++ src/main/java/com/suita/tarumi/SpringNihaoMemo/SpringNihaoMemoApplication.java
@@ -16,28 +16,6 @@ public class SpringNihaoMemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringNihaoMemoApplication.class, args);
-
-		// 外部ファイルからAWSサービスにアクセスするためのキー取得
-		final String RESOURCE_NAME = "application";
-		ResourceBundle rb = ResourceBundle.getBundle(RESOURCE_NAME);
-		final String accessKey = rb.getString("accesskey");
-		final String secretkey = rb.getString("secretkey");
-		final String REGION = rb.getString("region");
-
-		BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretkey);
-
-
-		AmazonTranslate translate = AmazonTranslateClient.builder()
-				.withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
-				.withRegion(REGION)
-				.build();
-
-		TranslateTextRequest request = new TranslateTextRequest()
-				.withText("私はプログラマー")
-				.withSourceLanguageCode("ja")
-				.withTargetLanguageCode("zh");
-		TranslateTextResult result  = translate.translateText(request);
-		System.out.println(result.getTranslatedText());
 	}
 
 }
